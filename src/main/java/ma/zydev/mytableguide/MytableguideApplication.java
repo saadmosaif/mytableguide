1c83ea32f4e468b9b3fa74e576d5d5760b96e6ca
saadmosaif <157498425+saadmosaif@users.noreply.github.com>
2025-10-15 12:59:04 +0100

    Initial commit

---
+++ src/main/java/ma/zydev/mytableguide/MytableguideApplication.java
@@ -0,0 +1,13 @@
+package ma.zydev.mytableguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytableguideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MytableguideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/zydev/mytableguide/MytableguideApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.zydev.mytableguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytableguideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
